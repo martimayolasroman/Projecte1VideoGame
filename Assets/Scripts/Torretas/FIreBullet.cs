3508fa7633fa95fc4f8069ca81b16913d1523f1f
DavidSeguer <david.seguer@enti.cat>
2021-03-11 09:36:28 +0100

    gitflow-feature-stash: Buttons&Torrets

---
+++ Assets/Scripts/Torretas/FIreBullet.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FIreBullet : MonoBehaviour
+{
+
+    public float speed;
+    [Header("Seconds of bullet life time")]
+    public float lifeTime;
+
+
+    public ParticleSystem destroyEffect;
+
+    public void SetUp(Vector2 shootingDir)
+    {
+
+
+        Invoke("DestroyBullet", lifeTime);
+        GetComponent<Rigidbody2D>().AddForce(shootingDir.normalized * speed, ForceMode2D.Impulse);
+        //FindObjectOfType<AudioManager>().Play("attack");
+
+    }
+
+
+    void DestroyBullet()
+    {
+        //Iniciem efecte
+        //Instantiate(destroyEffect, transform.position, transform.rotation);
+        //Destruim la bala
+        Destroy(gameObject);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+
+        if (collision.gameObject.tag == "Player")
+        {
+         
+            DestroyBullet();
+            
+        }
+
+        if (collision.gameObject.tag == "Ground")
+        {
+
+            DestroyBullet();
+        }
+    }
+
+
+}
---
+++ Assets/Scripts/UI/ChangeRunSprite.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+
+
+public class ChangeRunSprite : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+  
+
+    SwitchCharacter sw;
+    public Sprite dragon;
+    public Sprite knight;
+
+    void Start()
+    {
+        sw = FindObjectOfType<SwitchCharacter>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (sw.dragonn)
+        {
+            GetComponent<Image>().sprite = knight;
+        }
+        else
+        {
+            GetComponent<Image>().sprite = dragon;
+
+        }
+
+    }
+ 
+  
+}
