1da736cf5baa4c8a8dc74bef00378b8c12bd9f28
Marti <marti.mayolas@enti.cat>
2021-03-19 09:18:12 +0100

    gitflow-feature-stash: camFollowPlayer

---
+++ Assets/Scripts/Camera/CameraFollow.cs
@@ -0,0 +1,23 @@
+﻿
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+
+
+    public Transform target;
+    public float smoothSpeed =.123f;
+    public Vector3 offset;
+
+
+    private void LateUpdate()
+    {
+
+        transform.position = target.position + offset;
+
+
+    }
+
+
+
+}
