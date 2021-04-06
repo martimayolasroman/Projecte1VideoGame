6aca6f67a4162b1aa9d520cc63130509210eda31
Marti <marti.mayolas@enti.cat>
2021-04-06 17:06:28 +0200

    DragibEnergyBar

---
+++ Assets/Scripts/OrbePoints.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OrbePoints : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public int orbeEnergyValue = 10;
+    public EnergyBar EnergyBarScript;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.tag == "Player")
+        {
+            EnergyBarScript.PlusEnergy(orbeEnergyValue);
+            gameObject.SetActive(false);
+        }
+    }
+}
