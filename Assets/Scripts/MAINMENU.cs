24b457fcf472f88f25b0438226ff9afe3492170f
Salxi8 <marcelsetogomz8@gmail.com>
2021-04-09 12:23:28 +0200

    PAUSE

--- Assets/Scripts/MAINMENU.cs
+++ Assets/Scripts/MAINMENU.cs
@@ -17,6 +17,16 @@ public class MAINMENU : MonoBehaviour
         
     }
 
+    public void Pause()
+    {
+        Time.timeScale = 0;
+    }
+    public void Resume()
+    {
+        Time.timeScale = 1;
+
+    }
+
     public void escenaPlay()
     {
         SceneManager.LoadScene("LevelSelector");
@@ -25,5 +35,17 @@ public class MAINMENU : MonoBehaviour
     public void Exit()
     {
         Application.Quit();
+    }
+
+    public void MainMenu()
+    {
+        Time.timeScale = 1;
+        SceneManager.LoadScene("MENU");
+    }
+
+    public void Reload()
+    {
+        Time.timeScale = 1;
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
     }
 }
