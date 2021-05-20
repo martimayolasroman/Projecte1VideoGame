314ddf26b0e6b92764f223e9aa3d558969784613
Marti <marti.mayolas@enti.cat>
2021-05-20 10:41:52 +0200

    DistanciaFoc

---
+++ Assets/Scripts/DistFoc.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro ;
+
+public class DistFoc : MonoBehaviour
+{
+
+    public GameObject player;
+    public GameObject fire;
+    int dist;
+    TMP_Text distNum;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        distNum = gameObject.GetComponent<TMP_Text>();
+        player = GameObject.FindGameObjectWithTag("Player");
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+
+        dist = CalculateDist();
+
+        if(dist <= 0)
+        {
+            distNum.text = " ";
+
+        }
+        else {
+            distNum.text = dist + "m";
+        }
+        
+
+        
+
+        
+    }
+
+    public int CalculateDist()
+    {
+
+        Vector3 distancia;
+
+        distancia.x = (player.transform.position.x - fire.transform.position.x);
+
+        
+
+
+        return (int)distancia.x;
+
+    }
+
+
+}
